e8b0754e8174ae720cf3e25562b84d1ba368e0f2
Angela <nyabutiangella@gmail.com>
2024-09-15 09:04:28 +0300

    Introduction to dart variable definition

---
+++ playground.dart
@@ -0,0 +1,9 @@
+//dart:core - stores dart libraries
+import 'dart:core';
+main() {
+  //you can assign any value
+  var firstName = 'Angela';
+  //you cannot assign any other value apart from a string
+  String lastName = 'Nyabuti';
+  print( firstName + ' ' + lastName);
+}

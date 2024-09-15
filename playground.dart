b17e17475aff6104d5dd3e79fea5f8a466906789
Angela <nyabutiangella@gmail.com>
2024-09-15 16:38:36 +0300

    Datatypes

--- playground.dart
+++ playground.dart
@@ -1,9 +1,12 @@
-//dart:core - stores dart libraries
-//collecting input from a user
-import 'dart:io';
+//datatypes
 
 void main(List<String> args) {
-  stdout.writeln('What is your name: ?');  
-  String? name = stdin.readLineSync(); //? allows to receive a null value from the user 
-  print('My name is $name');
+ /*
+ int
+ double
+ string
+ bool
+ dynamic
+  */
 }
+

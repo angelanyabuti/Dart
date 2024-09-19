e482d320a8cd97bb856f0533a43bd4b40b83a101
Angela <nyabutiangella@gmail.com>
2024-09-19 23:35:12 +0300

    Loops

--- playground.dart
+++ playground.dart
@@ -1,16 +1,41 @@
-//Conditional Statement
-//if else
-//switch case
-void main(List<String> args) {
-  int number = 99;
+//loops
+//1. Standard for loop - similar to javascript
+//2. for-in loop
+//3. forEach Loop - hire order function takes another function as parameter
+//4. while loop
+//5. do...while loop
 
-  if ( number % 2 == 0) {
-    print('Even');
-  } else if( number % 3 == 0){
-    print('Odd');
-  } else {
-    print('confused');
+///break statements used to terminate the loop
+///break and continue
+void main(List<String> args) { 
+  for (var i=1; i <= 10; i++) {
+    print(i);
   }
-  
+
+  var numbers = [1, 2, 3];
+
+  for (var n in numbers) {
+    print(n);
+  }
+
+  numbers.forEach( (n) => print(n));
+
+  int num = 5;
+
+  do{
+    print(num);
+    num -= 1;
+  }while (num > 0);
+
+  for (var i = 0; i < 10; i++){
+    if (i > 5) break;
+    print(i);
+  }
+
+  for (var i = 0; i < 10; i++) {
+    if (i % 2 == 0) continue;
+    print("Odd: $i");
+  }
+
 }
 
